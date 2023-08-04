9654559f673e84194536b962e69eb52b0236ff98
rlypovskyi <121307948+rlypovskyi@users.noreply.github.com>
2023-08-03 23:01:51 -0400

    1

--- js/modal.js
+++ js/modal.js
@@ -1,61 +1,21 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    modalCheckbox: document.querySelector("#modal-form-checkbox"),
-    modalForm: document.querySelector(".modal-form"),
-    nameInput: document.querySelector("#user_name"),
-    phoneInput: document.querySelector("#user_phone"),
-    emailInput: document.querySelector("#user_email"),
-    commentInput: document.querySelector("#user_comment"),
-  };
+// Функція, яка викликається після завантаження сторінки
+document.addEventListener("DOMContentLoaded", function() {
+  // Отримання посилань на елементи, які будемо використовувати
+  var openModalBtn = document.querySelector("[data-modal-open]");
+  var closeModalBtn = document.querySelector("[data-modal-close]");
+  var modal = document.querySelector("[data-modal]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  refs.modalCheckbox.addEventListener('change', () => {
-    refs.modalForm.querySelector('button[type="submit"]').disabled = !refs.modalCheckbox.checked;
-  });
-
-  refs.modalForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let nameRegEx = /^[^\p{P}\p{S}\p{C}\p{N}]+$/u;
-    let phoneRegEx = /^\d{10,14}$/;
-
-    if (!nameRegEx.test(refs.nameInput.value)) {
-      alert("Please check the name. It should contain only letters.");
-      return false;
-    }
-
-    if (!phoneRegEx.test(refs.phoneInput.value)) {
-      alert("Please check the phone number. It should be in international format without + and between 10 to 14 digits.");
-      return false;
-    }
-
-    if (refs.commentInput.value.trim() === "") {
-      alert("Please enter your message.");
-      return false;
-    }
-
+  // Додавання обробників подій для відкриття та закриття модального вікна
+  openModalBtn.addEventListener("click", function() {
     toggleModal();
-
-    return true;
   });
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  closeModalBtn.addEventListener("click", function() {
+    toggleModal();
+  });
 
-    if(refs.modal.classList.contains("is-hidden")) {
-        // Clear the form inputs
-        refs.nameInput.value = "";
-        refs.phoneInput.value = "";
-        refs.emailInput.value = "";
-        refs.commentInput.value = "";
-        refs.modalCheckbox.checked = false;
-        // Disable the submit button
-        refs.modalForm.querySelector('button[type="submit"]').disabled = true;
-    }
+  // Функція для перемикання модального вікна
+  function toggleModal() {
+    modal.classList.toggle("is-hidden");
   }
-})();
+});
